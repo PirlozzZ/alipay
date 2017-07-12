c2a245553c876896cdefc17a923ea814c5db6c20
Len <liuzhen0927@gmail.com>
2017-07-12 17:21:31 +0800

    modified by Len

--- Project_alipay/alipay.WebPage/TestPage.aspx.cs
+++ Project_alipay/alipay.WebPage/TestPage.aspx.cs
@@ -3,6 +3,7 @@ using Aop.Api.Request;
 using Aop.Api.Response;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -15,20 +16,39 @@ namespace alipay.WebPage
         log4net.ILog log = log4net.LogManager.GetLogger("Log.Logging");
         protected void Page_Load(object sender, EventArgs e)
         {
-            string code = Request.QueryString["code"];
+            string code = Request.QueryString["auth_code"];
             string state = Request.QueryString["state"];
             log.Info("code:" + code + "——state:" + state);
             if (!string.IsNullOrEmpty(code))
             {
-                IAopClient client = new DefaultAopClient("https://openapi.alipay.com/gateway.do", "2017062307553030", "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxC63kbKWX++F5SmlFP3/L/fOvCnnhon7psxgGFqw+n0UkTgFNf6MlqaF2l43JacP/At3i5vyMbX/yFcoDW+mNIeLOhrHBoKYbu1mVSQEcW82LLAlasVKM6i0wCmBstlMYmbjInvhYG8n5pkcMPzsgVmSX8h07qFt2sP/Jkz6TVmRmvKKfT2ebT+3vgPvLgcmtl4iEZDvPyhbOqzAdCVh2OpuSym//y74NpxEzZ1/Ml0qoxCmtdmLuaDWv/0pUbcUpmYalr1H9Wvmxd2P7WdSO74iUSOC7bV/yZE82kH8SLKA07cK9nWokhllS/kUPb0uzlhUE93hkKurxS5QU4RR5QIDAQAB", "json", "1.0", "RSA2", "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAofT80EKaSXQIsGqsdpLORJZ2a/EhZNI+5aBoJ2pusjo7F5bxJpPEiii4H09yyHDQKnhcuDfEXNjhX8vpPV5umtFPNfo1PRx/FpSEG2kPDmBKITNd6KotvGKhCouhjsE+WLQbpmLe72J1hpkR8LRn4LH4qnOPRt7wX9Avw0rB9eEUKTy6jbSgFCIv/Nqa5MDKouTI/Hu4/Quf37gYYfz3u7rzl5V8y9bJKofSzxNm9afN8xgUas7abBtev5edX960/t6wq9Rglr97yKxy7FdQN98JYg7wHZHTi+nRjRNtYdKr5Q3sdW85X1yH/ZxpAicCf1eoScrFxIGpumvybPI+HQIDAQAB", "GBK", false);
-                AlipayOpenAuthTokenAppRequest request = new AlipayOpenAuthTokenAppRequest();
-                request.BizContent = "{" +
-                "\"grant_type\":\"authorization_code\"," +
-                "\"code\":\"" + code + "\"," +
-                "  }";
-                AlipayOpenAuthTokenAppResponse response = client.Execute(request);
-                log.Info(response.Body);
+                try
+                {
+                    string publicKey = ConfigurationManager.AppSettings["publicKey"];
+                    string privateKey = ConfigurationManager.AppSettings["privateKey"];
+                    string publicKeyPem = GetCurrentPath() + "public-key.pem";
+                    string privateKeyPem = GetCurrentPath() + "aop-sandbox-RSA-private-c#.pem";
+                    log.Info("publicKeyPem:" + publicKeyPem);
+                    log.Info("privateKeyPem:" + privateKeyPem);
+                    IAopClient client = new DefaultAopClient("https://openapi.alipay.com/gateway.do", "2017062307553030", privateKey, "json", "1.0", "RSA", publicKey, "GBK", false);
+                    AlipaySystemOauthTokenRequest request = new AlipaySystemOauthTokenRequest();
+                    request.GrantType = "authorization_code";
+                    request.Code = code;
+                    //request.RefreshToken = "201208134b203fe6c11548bcabd8da5bb087a83b";
+                    AlipaySystemOauthTokenResponse response = client.Execute(request);
+                    Console.WriteLine(response.Body);
+                    log.Info(response.Body);
+                }
+                catch (Exception err)
+                {
+                    log.Error("err:", err);
+                }
             }
+        }
+
+        private static string GetCurrentPath()
+        {
+            string basePath = System.AppDomain.CurrentDomain.BaseDirectory;
+            return basePath + @"Key\";
         }
     }
 }
